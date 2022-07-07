6f057220d73e81d0a36b88eaf0ea65c1f0a63863
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2022-07-07 09:57:43 +0530

    coded

--- src/main/java/com/java/data/JavaSocketExample1.java
+++ src/main/java/com/java/data/JavaSocketExample1.java
@@ -6,19 +6,20 @@ public class JavaSocketExample1 {
 		int port = 1085;
 		InetAddress inetAddress = InetAddress.getByName("localhost");
 		System.out.println(inetAddress);
-		//this is used to create the dataGram Sockets Objects
-		DatagramSocket  dataGramSocket = new DatagramSocket();
-		//Connects method is used to connect the inetAddress and port Number With this Sockets
-		dataGramSocket.connect(inetAddress,port);
-		//it returns null if the sockets is closed 
-		System.out.println("the remote Sockets Address is " + dataGramSocket.getRemoteSocketAddress());
-		
-		//print the local port NUmber if sockets is bound
-		System.out.println("the Local Port number " + dataGramSocket.getLocalPort());
-		//print the local Socket Address 
-		System.out.println("the LocalSocket Address is " + dataGramSocket.getLocalAddress());
-		//getting the port Number on which the Sockets is bound
-		System.out.println("the port Number is " + dataGramSocket.getPort());
+		try (//this is used to create the dataGram Sockets Objects
+		DatagramSocket dataGramSocket = new DatagramSocket()) {
+			//Connects method is used to connect the inetAddress and port Number With this Sockets
+			dataGramSocket.connect(inetAddress,port);
+			//it returns null if the sockets is closed 
+			System.out.println("the remote Sockets Address is " + dataGramSocket.getRemoteSocketAddress());
+			
+			//print the local port NUmber if sockets is bound
+			System.out.println("the Local Port number " + dataGramSocket.getLocalPort());
+			//print the local Socket Address 
+			System.out.println("the LocalSocket Address is " + dataGramSocket.getLocalAddress());
+			//getting the port Number on which the Sockets is bound
+			System.out.println("the port Number is " + dataGramSocket.getPort());
+		}
 	}
 
 }
